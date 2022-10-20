d411ceb059211746058fafa24c59934723a16b49
Galina Shchepotina <113752322+galsepo@users.noreply.github.com>
2022-10-19 19:24:18 -0700

    update images

--- src/components/directory/directory.component.tsx
+++ src/components/directory/directory.component.tsx
@@ -17,31 +17,31 @@ const categories: DirectoryCategory[] = [
   {
     id: 101,
     title: 'living room',
-    imageUrl: 'https://i.ibb.co/GMnGBH5/sofas.jpg',
+    imageUrl: 'https://images.unsplash.com/photo-1615873968403-89e068629265?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2064&q=80',
     route: 'shop/livingrooms',
   },
   {
     id: 201,
     title: 'kitchen',
-    imageUrl: 'https://i.ibb.co/PhFj76m/kitchen.jpg',
+    imageUrl: 'https://images.unsplash.com/photo-1611095210561-67f0832b1ca3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80',
     route: 'shop/kitchen',
   },
   {
     id: 301,
     title: 'bedrooms',
-    imageUrl: 'https://i.ibb.co/FXLvm7w/bedrooms.jpg',
+    imageUrl: 'https://images.unsplash.com/photo-1595526114035-0d45ed16cfbf?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80',
     route: 'shop/bedrooms',
   },
   {
     id: 401,
     title: 'dining',
-    imageUrl: 'https://i.ibb.co/djmJFK1/diningroom.jpg',
+    imageUrl: 'https://images.unsplash.com/photo-1617806118233-18e1de247200?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2064&q=80',
     route: 'shop/dining',
   },
   {
     id: 501,
     title: 'bathroom',
-    imageUrl: 'https://i.ibb.co/KKLt7YS/bathroom.jpg',
+    imageUrl: 'https://plus.unsplash.com/premium_photo-1661956103157-556678e6dafd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2148&q=80',
     route: 'shop/bathroom',
   },
 ];
